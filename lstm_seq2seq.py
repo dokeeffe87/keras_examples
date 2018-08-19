64b654107b5cfbf780aa183c90d2201167661322
Dan <okeeffed090@gmail.com>
2018-08-19 01:04:43 -0400

    more complete example.

--- lstm_seq2seq.py
+++ lstm_seq2seq.py
@@ -89,7 +89,7 @@ decoder_outputs = decoder_dense(decoder_outputs)
 model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 
 # Start the training
-model.compile(optimizer='rmsprop', loss='categorical_crossentr        opy')
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
 model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
           batch_size=batch_size, epochs=epochs, validation_split=0.2)
 # Save the trained model
